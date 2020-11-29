441c8763f39c9899c8134d9ad1f584c4498a313a
justinelijahtaylor <justinelijahtaylor@gmail.com>
2020-11-29 13:28:39 -0500

    Removing unwanted files

--- build/static/js/53.106275b7.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpTreblecross=this.webpackJsonpTreblecross||[]).push([[53],{133:function(n,t,e){"use strict";e.r(t),e.d(t,"startStatusTap",(function(){return r}));var o=e(11),r=function(){var n=window;n.addEventListener("statusTap",(function(){Object(o.h)((function(){var t=n.innerWidth,e=n.innerHeight,r=document.elementFromPoint(t/2,e/2);if(r){var c=r.closest("ion-content");c&&c.componentOnReady().then((function(){Object(o.f)((function(){return c.scrollToTop(300)}))}))}}))}))}}}]);
-//# sourceMappingURL=53.106275b7.chunk.js.map
